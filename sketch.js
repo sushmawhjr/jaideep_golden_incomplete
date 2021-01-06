cf82175aad319e5ab1e4271555ba14e5316fbe48
sushmawhjr <67134178+sushmawhjr@users.noreply.github.com>
2021-01-06 22:58:20 +0530

    jumping on collid

--- sketch.js
+++ sketch.js
@@ -2,14 +2,16 @@
 
  var lep_running, lep;
  var ground_img,ground;
- var hWood, wood_img;
+ var hWood, wood_img, HWoodGroup;
+ var bg;
  
 
  function preload(){
     lep_running =  loadAnimation("images/l1.png","images/l2.png","images/l3.png");
     lep_running_reverse =  loadAnimation("images/l1flip.png","images/l2flip.png","images/l3flip.png");
-    ground_img  =  loadImage("images/grass1.png");
+    ground_img  =  loadImage("images/grass.png");
     wood1  = loadImage("images/EWood.png");
+    bg  = loadImage("images/bg.png");
  }
  function setup(){
     createCanvas(displayWidth,displayHeight-150);
@@ -20,29 +22,37 @@
 
     ground   = createSprite(width/2, height-100,width,30);
     ground.addImage("ground",ground_img);
+    ground.x  = ground.width / 2;
+   // ground.velocityX  = -2;
 
     lep = createSprite(50,height-150);
     lep.addAnimation("run", lep_running);
-    lep.addAnimation("reverse",lep_running_reverse)
+    lep.addAnimation("reverse",lep_running_reverse);
+
+    HWoodGroup = new Group ();
 
 //     invisibleGround  = createSprite(width/2, height-1,width,5)
 //    invisibleGround.visible = false;
-    
-   hWood  = createSprite(width/2,height-160);
-   hWood.addImage("wood1",wood1);
-   hWood.scale = 0.8
-   //lep.debug  = true;
-   lep.setCollider("rectangle",0,0,70,70)
 
-   
+horizontalWood(150);
+horizontalWood(500);
+
 
  }
 
  function draw(){
 
-    background("white");
+    background(bg);
     lep.collide(ground);
 
+    
+   // hWood(350);
+ 
+    //hWood(700);
+
+    if(ground.x < 0){
+       ground.x = ground.width/2
+    }
 
     if(keyDown("space")){
         lep.velocityY  = -10;
@@ -54,8 +64,9 @@
         lep.changeAnimation("run", lep_running);
     }
 
-   if(lep.isTouching(hWood)){
-    lep.collide(hWood);
+   if(lep.isTouching(HWoodGroup)){
+    lep.collide(HWoodGroup);
+    //lep.velocityY = -3;
    }
 
    if(keyDown("left")){
@@ -63,5 +74,16 @@
     lep.changeAnimation("reverse",lep_running_reverse)
    }
     drawSprites();
+
+ }
+
+
+ function horizontalWood(x){
+   hWood  = createSprite(x,height-160);
+   hWood.addImage("wood1",wood1);
+   hWood.scale = 0.8
+   //lep.debug  = true;
+   lep.setCollider("rectangle",0,0,70,70);
+   HWoodGroup.add(hWood);
 
  }
